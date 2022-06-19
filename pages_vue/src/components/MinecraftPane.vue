d49925c1691f0151fc7f9d21fa79094463b8413a
cccccc <ccccccminecraft62@gmail.com>
2022-06-19 18:32:12 +0900

    minecraftサーバー状況ページが見れる

--- pages_vue/src/components/MinecraftPane.vue
+++ pages_vue/src/components/MinecraftPane.vue
@@ -1,12 +1,73 @@
 <template>
-<div>
-    <p>サーバー状況</p>
-    ---test---
+<div class="main">
+    <h1>サーバー状況</h1>
+    <div class="box">
+        <p>疲れた！</p>
+        <p>デザインはまた今度</p>
+        <table>
+            <tr>
+                <th>サーバー名</th>
+                <th>アドレス</th>
+                <th>人数</th>
+                <th>ping</th>
+            </tr>
+            <tr v-for="(server, key) in this.servers" :key="key">
+                <td>{{server.server_name}}</td>
+                <td>{{server.global_address}}</td>
+                <td>{{server.players}}</td>
+                <td>{{server.ping}}</td>
+            </tr>
+        </table>
+        <div class="top">
+            <router-link to="/">戻る</router-link>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
-</script>
+import axios from "axios";
 
+export default{
+    data: function(){
+        return {
+            servers: undefined
+        }
+    },
+    created: function(){
+        this.getStatus()
+    },
+    methods: {
+        getStatus(){
+            const url = "http://localhost:5000/minecraft"
+            axios
+                .get(url)
+                .then(responce=>(this.servers = responce.data))
+        }
+    }
+}
+</script>
 <style>
+.box{
+    width: clamp(100px,90%,100vh);
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+table{
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+}
+th{
+    border: 2px #cccccc solid;
+}
+td{
+    border: 2px #cccccc solid;
+}
+.top{
+    text-align: center;    
+    margin-left: auto;
+    margin-right: auto;
+}
 </style>
--- pages_vue/src/router.js
+++ pages_vue/src/router.js
@@ -1,12 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Top from './components/top.vue'
 import MinecraftPane from './components/MinecraftPane.vue'
-//
-// 他のコンポーネントは省略
-//
 
 export default createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes: [
         {
             path: '/',
